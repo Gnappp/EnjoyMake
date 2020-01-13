21e9b7ae559c36095310e11090b0d60cd2b83ac8
Gnappp <sakuya_v@naver.com>
2020-01-13 17:26:30 +0900

    Boss design

---
+++ New Unity Project/Assets/Script/Boss.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour
+{
+
+    private int hp;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
