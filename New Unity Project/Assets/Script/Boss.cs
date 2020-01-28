b374fbf5b9bb8850d8f82e15d50a55943a119d32
Gnappp <sakuya_v@naver.com>
2020-01-28 17:52:21 +0900

    Making solar

--- New Unity Project/Assets/Script/Boss.cs
+++ New Unity Project/Assets/Script/Boss.cs
@@ -6,25 +6,32 @@ public class Boss : MonoBehaviour
 {
     public float jump_delay = 4f;
     public GameObject seed;
+    public GameObject solar;
 
-    private int hp;
+    private int hp = 50;
     private GameObject player;
     private bool jumping = true;
     private float jump_time = 0f;
+    private float createseedTime = 0f;
+    private float freezeTime = 0f;
     private Rigidbody2D rb2d;
+    private bool solarPhase = false;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
         rb2d = gameObject.GetComponent<Rigidbody2D>();
-        CreateSeed();
+        if (gameObject.name != "Boss_Monster_Plant(Clone)")
+            hp = 25;
     }
 
     // Update is called once per frame
     void Update()
     {
         Jump();
+        CreateSeed();
+        Phase2();
     }
 
     void Jump()
@@ -43,7 +50,8 @@ public class Boss : MonoBehaviour
         if (jump_time > jump_delay && !jumping)
         {
             float ptr_pos_x = player.transform.position.x - gameObject.transform.position.x;
-            rb2d.AddForce(new Vector2(ptr_pos_x / 1.5f, 8f), ForceMode2D.Impulse);
+            rb2d.AddForce(new Vector2(ptr_pos_x / 1.5f, 7.5f), ForceMode2D.Impulse);
+            Debug.Log(ptr_pos_x / 1f + 8f);
             jump_time = 0f;
             jumping = true;
         }
@@ -57,7 +65,36 @@ public class Boss : MonoBehaviour
 
     void CreateSeed()
     {
-        GameObject inst = Instantiate(seed) as GameObject;
-        inst.transform.GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f, 0f);
+        if (createseedTime > 5 && gameObject.name != "Boss_Monster_Plant(Clone)")
+        {
+            GameObject inst = Instantiate(seed) as GameObject;
+            inst.transform.GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f, 0f);
+            createseedTime = 0f;
+        }
+        else
+            createseedTime += Time.deltaTime;
+    }
+    void Phase2()
+    {
+        if (hp < 25 && gameObject.name != "Boss_Monster_Plant(Clone)" && !solarPhase)
+        {
+            if (jumping && rb2d.velocity.y < -0.1f)
+            {
+                rb2d.constraints = RigidbodyConstraints2D.FreezeAll;
+                //GameObject inst = Instantiate(solar) as GameObject;
+                //inst.transform.position = gameObject.transform.position;
+                solarPhase = true;
+            }
+        }
+        if (rb2d.constraints == RigidbodyConstraints2D.FreezeAll)
+        {
+            if (freezeTime > 2f)
+            {
+                rb2d.constraints = RigidbodyConstraints2D.None;
+                rb2d.freezeRotation = true;
+            }
+            else
+                freezeTime += Time.deltaTime;
+        }
     }
 }
--- New Unity Project/Assets/Script/Bush.cs
+++ New Unity Project/Assets/Script/Bush.cs
@@ -5,8 +5,20 @@ using UnityEngine.Tilemaps;
 
 public class Bush : MonoBehaviour
 {
+    class BushInObject
+    {
+        public Vector3Int objectPos { get; set; }
+        public GameObject obj { get; set; }
+        public BushInObject(Vector3Int pos,GameObject gameobject)
+        {
+            objectPos = pos;
+            obj = gameobject;
+        }
+    }
+
     public GameObject mushroom;
 
+    private HashSet<BushInObject> hideBushObjects;
     private Tilemap tileMap;
     private Vector3Int monsterPos = Vector3Int.zero;
     private Vector3Int playerPos = Vector3Int.zero;
@@ -16,28 +28,29 @@ public class Bush : MonoBehaviour
     void Start()
     {
         tileMap = gameObject.GetComponent<Tilemap>();
+        hideBushObjects = new HashSet<BushInObject>();
     }
 
     // Update is called once per frame
     void Update()
     {
     }
-       
+
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         EnterBush(collision);
-        if(collision.gameObject.layer==8)
-        {
-            if (enterBushTime > 4f)
-            {
-                GameObject inst = Instantiate(mushroom) as GameObject;
-                inst.transform.position = collision.transform.position;
-                enterBushTime = 0f;
-            }
-            else
-                enterBushTime += Time.deltaTime;
-        }
+        //if (collision.gameObject.layer == 8)
+        //{
+        //    if (enterBushTime > 4f)
+        //    {
+        //        GameObject inst = Instantiate(mushroom) as GameObject;
+        //        inst.transform.position = collision.transform.position;
+        //        enterBushTime = 0f;
+        //    }
+        //    else
+        //        enterBushTime += Time.deltaTime;
+        //}
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -45,101 +58,74 @@ public class Bush : MonoBehaviour
         ExitBush(collision);
     }
 
-    void EnterBush(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.transform.tag == "Monster")
+        if (collision.gameObject.tag == "Monster")
         {
-            Vector3Int cellPos = tileMap.WorldToCell(collision.transform.position);
-            if (monsterPos == Vector3Int.zero && tileMap.GetTile(cellPos) != null)
+            hideBushObjects.Add(new BushInObject(Vector3Int.zero, collision.gameObject));
+        }
+        if(collision.gameObject.tag == "Player")
+        {
+            if(collision.GetType()==typeof(CapsuleCollider2D))
             {
-                monsterPos = cellPos;
-            }
-            if (monsterPos == Vector3Int.zero && tileMap.GetTile(cellPos) == null)
-            {
-                if (cellPos.y == -1)
-                {
-                    Vector3Int ptr_cellPos = cellPos;
-                    Vector3Int ptr_cellPos1 = cellPos;
-                    ptr_cellPos.x -= 1;
-                    ptr_cellPos1.x += 1;
-
-                    if (tileMap.GetTile(ptr_cellPos) != null)
-                    {
-                        tileMap.SetTileFlags(ptr_cellPos, TileFlags.None);
-                        tileMap.SetColor(ptr_cellPos, Color.green);
-                        monsterPos = ptr_cellPos;
-                    }
-                    else if (tileMap.GetTile(ptr_cellPos1) != null)
-                    {
-                        tileMap.SetTileFlags(ptr_cellPos1, TileFlags.None);
-                        tileMap.SetColor(ptr_cellPos1, Color.green);
-                        monsterPos = ptr_cellPos1;
-                    }
-                }
-            }
-
-            if (Mathf.Abs(monsterPos.x - cellPos.x) == 0 && tileMap.GetTile(cellPos) != null)
-            {
-                tileMap.SetTileFlags(cellPos, TileFlags.None);
-                tileMap.SetColor(cellPos, Color.green);
-                monsterPos = cellPos;
-            }
-            else if (Mathf.Abs(monsterPos.x - cellPos.x) > 1 && tileMap.GetTile(cellPos) != null)
-            {
-                tileMap.SetTileFlags(monsterPos, TileFlags.None);
-                tileMap.SetColor(monsterPos, new Color(255f, 255f, 255f, 1f));
-
-                tileMap.SetTileFlags(cellPos, TileFlags.None);
-                tileMap.SetColor(cellPos, Color.green);
-                monsterPos = cellPos;
+                hideBushObjects.Add(new BushInObject(Vector3Int.zero, collision.gameObject));
             }
         }
-        if (collision.transform.tag == "Player")
-        {
-            Vector3Int cellPos = tileMap.WorldToCell(collision.transform.position);
-            if (playerPos == Vector3Int.zero && tileMap.GetTile(cellPos) != null)
-            {
-                playerPos = cellPos;
-            }
-            if (playerPos == Vector3Int.zero && tileMap.GetTile(cellPos) == null)
-            {
-                if (cellPos.y == -1)
-                {
-                    Vector3Int ptr_cellPos = cellPos;
-                    Vector3Int ptr_cellPos1 = cellPos;
-                    ptr_cellPos.x -= 1;
-                    ptr_cellPos1.x += 1;
+    }
 
-                    if (tileMap.GetTile(ptr_cellPos) != null)
+    void EnterBush(Collider2D collision)
+    {
+        foreach (BushInObject hidebush in hideBushObjects)
+        {
+            if (hidebush.obj.gameObject == collision.gameObject)
+            {
+                Vector3Int cellPos = tileMap.WorldToCell(collision.transform.position);
+                if (hidebush.objectPos == Vector3Int.zero && tileMap.GetTile(cellPos) != null)
+                {
+                    hidebush.objectPos = cellPos;
+                }
+                if (hidebush.objectPos == Vector3Int.zero && tileMap.GetTile(cellPos) == null)
+                {
+                    if (cellPos.y == -1)
                     {
-                        tileMap.SetTileFlags(ptr_cellPos, TileFlags.None);
-                        tileMap.SetColor(ptr_cellPos, Color.green);
-                        playerPos = ptr_cellPos;
+                        Vector3Int ptr_cellPos = cellPos;
+                        Vector3Int ptr_cellPos1 = cellPos;
+                        ptr_cellPos.x -= 1;
+                        ptr_cellPos1.x += 1;
+
+                        if (tileMap.GetTile(ptr_cellPos) != null)
+                        {
+                            tileMap.SetTileFlags(ptr_cellPos, TileFlags.None);
+                            tileMap.SetColor(ptr_cellPos, Color.green);
+                            hidebush.objectPos = ptr_cellPos;
+                        }
+                        else if (tileMap.GetTile(ptr_cellPos1) != null)
+                        {
+                            tileMap.SetTileFlags(ptr_cellPos1, TileFlags.None);
+                            tileMap.SetColor(ptr_cellPos1, Color.green);
+                            hidebush.objectPos = ptr_cellPos1;
+                        }
                     }
-                    else if (tileMap.GetTile(ptr_cellPos1) != null)
-                    {
-                        tileMap.SetTileFlags(ptr_cellPos1, TileFlags.None);
-                        tileMap.SetColor(ptr_cellPos1, Color.green);
-                        playerPos = ptr_cellPos1;
-                    }
+                }
+
+                if (Mathf.Abs(hidebush.objectPos.x - cellPos.x) == 0 && tileMap.GetTile(cellPos) != null)
+                {
+                    tileMap.SetTileFlags(cellPos, TileFlags.None);
+                    tileMap.SetColor(cellPos, Color.green);
+                    hidebush.objectPos = cellPos;
+                    Debug.Log("gg");
+                }
+                else if (Mathf.Abs(hidebush.objectPos.x - cellPos.x) > 1 && tileMap.GetTile(cellPos) != null)
+                {
+                    tileMap.SetTileFlags(hidebush.objectPos, TileFlags.None);
+                    tileMap.SetColor(hidebush.objectPos, new Color(255f, 255f, 255f, 1f));
+
+                    tileMap.SetTileFlags(cellPos, TileFlags.None);
+                    tileMap.SetColor(cellPos, Color.green);
+                    hidebush.objectPos = cellPos;
                 }
             }
 
-            if (Mathf.Abs(playerPos.x - cellPos.x) == 0 && tileMap.GetTile(cellPos) != null)
-            {
-                tileMap.SetTileFlags(cellPos, TileFlags.None);
-                tileMap.SetColor(cellPos, Color.green);
-                playerPos = cellPos;
-            }
-            else if (Mathf.Abs(playerPos.x - cellPos.x) > 1 && tileMap.GetTile(cellPos) != null)
-            {
-                tileMap.SetTileFlags(playerPos, TileFlags.None);
-                tileMap.SetColor(playerPos, new Color(255f, 255f, 255f, 1f));
-
-                tileMap.SetTileFlags(cellPos, TileFlags.None);
-                tileMap.SetColor(cellPos, Color.green);
-                playerPos = cellPos;
-            }
         }
     }
 
@@ -157,6 +143,16 @@ public class Bush : MonoBehaviour
             tileMap.SetColor(playerPos, new Color(255f, 255f, 255f, 1f));
             playerPos = Vector3Int.zero;
             enterBushTime = 0f;
+        }
+        foreach (BushInObject hidebush in hideBushObjects)
+        {
+            if (hidebush.obj == collision.gameObject)
+            {
+                tileMap.SetTileFlags(hidebush.objectPos, TileFlags.None);
+                tileMap.SetColor(hidebush.objectPos, new Color(255f, 255f, 255f, 1f));
+                hideBushObjects.Remove(hidebush);
+                break;
+            }
         }
     }
 
